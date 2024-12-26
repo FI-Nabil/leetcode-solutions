46a0832d8da29f6b6090e94f7df54fc0e8e9de8b
FI-Nabil <finabil98@gmail.com>
2024-12-27 02:43:22 +0600

    simple

---
+++ 128.longest-consecutive-sequence.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=128 lang=cpp
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if(nums.size() == 0){
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int ans = 1;
+        int cnt = 1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] - nums[i-1] == 1){
+                cnt++;
+                ans = max(ans, cnt);
+            }
+            else if(nums[i] - nums[i-1] == 0){
+                continue;
+            }
+            else{
+                cnt = 1;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
